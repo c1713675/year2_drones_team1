f73745c0ed277621584ab69b8b3790a966bbc5fc
James Buckland <BucklandJ2@cardiff.ac.uk>
2018-11-19 15:16:55 +0000

    added cookies to define access level to the website

--- Project/src/main/java/com/asgdrones/drones/controllers/LoginController.java
+++ Project/src/main/java/com/asgdrones/drones/controllers/LoginController.java
@@ -4,6 +4,7 @@ import com.asgdrones.drones.domain.Login;
 import com.asgdrones.drones.repositories.LoginRepoJPA;
 import com.asgdrones.drones.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +39,9 @@ public class LoginController {
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public ModelAndView postLogin(@Valid Login login,
-                                  BindingResult bindingResult, Model model) {
+                                  BindingResult bindingResult,
+                                  HttpServletResponse response,
+                                  Model model) {
         if (bindingResult.hasErrors()) {
             System.out.println("Validation error" + bindingResult.getFieldErrors());
         } else {
@@ -45,6 +50,7 @@ public class LoginController {
             System.out.println("access: "+access);
             System.out.println("username: "+login.getUsername());
             System.out.println("password: "+login.getPassword());
+            response.addCookie(new Cookie("Access",access));
         }
         model.addAttribute("login", login);
         return new ModelAndView("landingPage", model.asMap());
